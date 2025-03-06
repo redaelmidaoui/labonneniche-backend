e688772b55248043fa53f97d70a9dcb5b51f57b8
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-03-06 18:09:36 +0100

    fix app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('./models/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
