1e807246cac097bc7aa1c4555e104a9bb81cd588
Réda <r.elmidaoui@outlook.fr>
2025-03-13 16:29:29 +0100

    connection

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
+require('./models/connection');
 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./models/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
